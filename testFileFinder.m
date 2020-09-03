3f34fb3da6346c465d4e034ee55d3f113bdd2471
JBKacerovsky <kacerovsky@gmail.com>
2020-09-03 04:00:00 -0400

    removed obsolete testing files

--- testFileFinder.m
+++
@@ -1,5 +0,0 @@
-function XfigureExport(figureTitle)
-    path = fileparts(mfilename('fullpath'));
-    path = stcat(path, '/MatLabViewer.app/Contents/Resources/Data/StreamingAssets/', figureTitle);
-    
-end
